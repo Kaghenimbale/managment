6ce196b955e72898e2d0692e1e0f9102a7b23e83
Kagheni <Kaghenimbale@gmail.com>
2023-11-08 15:59:51 +0200

    Add project backend setup

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  delete :logout, to: 'session#logout'
+  get :logged_in, to: 'sessions#logged_in'
+  # Defines the root path route ("/")
+  root "static#home"
+end
